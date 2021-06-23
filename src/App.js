8d10027d6b24e539588afed0c4e9d285f63681ce
David <davidabutler92@gmail.com>
2021-06-22 17:44:26 -0700

    add Providers for muiTheme & redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+/** styles */
+
+/** material ui components */
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+
+/** components */
+import HomePage from './pages/HomePage/HomePage';
+import CatchPokemonPage from './pages/CatchPokemonPage/CatchPokemonPage';
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <main>
+      <CssBaseline />
+      <ThemeProvider>
+        <Router>
+          <Provider>
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/catchPokemon" component={CatchPokemonPage} />
+            </Switch>
+          </Provider>
+        </Router>
+      </ThemeProvider>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/pages/CatchPokemonPage/CatchPokemonPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CatchPokemonPage() {
+  return <div>Catch Pokemon Here</div>;
+}
